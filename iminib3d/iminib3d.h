948c6f2e9f87f1e484a0459c455ac77621ce7f40
simonh <si@si-design.co.uk>
2012-01-07 18:01:32 +0000

    Initial Commit

---
+++ iminib3d/iminib3d.h
@@ -0,0 +1,13 @@
+//
+//  iminib3d.h
+//  iminib3d
+//
+//  Created by Simon Harrison on 07/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iminib3d : NSObject
+
+@end
---
+++ iminib3d/iminib3d.m
@@ -0,0 +1,23 @@
+//
+//  iminib3d.m
+//  iminib3d
+//
+//  Created by Simon Harrison on 07/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "iminib3d.h"
+
+@implementation iminib3d
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
